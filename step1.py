45911201641161e95427ec44dbd2969fb30019ce
ymiquel <yannmiquel@live.fr>
2019-05-08 11:32:56 +0200

    First python script

---
+++ step1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
+
